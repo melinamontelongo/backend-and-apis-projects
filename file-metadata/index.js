1c38dd266c9eca98f52196e28250bc8647b73275
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-03-11 12:41:47 -0300

    add file metadata project

---
+++ file-metadata/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const multer  = require('multer');
+const upload = multer();
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
+  try{
+    let response = {
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size,
+    };
+    res.json(response);
+  } catch(e){
+    res.json({"error": "an error occurred while trying to upload your file"});
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
